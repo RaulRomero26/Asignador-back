1fe26e77076cf7ac6c04e29ef1d383a5d30de7b8
RaulRomero26 <link_2642@hotmail.com>
2024-07-08 12:49:56 -0600

    update log values

--- controllers/tareas.controller.js
+++ controllers/tareas.controller.js
@@ -383,7 +383,7 @@ const actualizarTarea = async (req, res) => {
         
         // Añadir el id al final de los valores
         values.push(id);
-
+        console.log(values)
         console.log(`UPDATE tareas SET ${setClause} WHERE id_tarea = ?`);
         const updateResult = await tareasPromisePool.query(`UPDATE tareas SET ${setClause} WHERE id_tarea = ?`, values);
         //console.log(updateResult);
